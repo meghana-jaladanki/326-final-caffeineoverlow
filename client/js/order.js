52e9a1796b4c2ce6bff990b95c4ba2989349ab8d
Mehul1107 <43730457+Mehul1107@users.noreply.github.com>
2022-04-20 21:22:35 -0400

    Updated order.js

--- client/js/order.js
+++ client/js/order.js
@@ -1,49 +1,52 @@
 "use strict";
 
-// await fetch(`/orders`, {
-// 			method: "GET",
-// 			headers: {
-// 				"Content-Type": "application/json",
-// 			},
-// 		})
-//     .then((response) => response.text())
-// 		.then((result) => {
-//       let new_item = document.getElementById("new_item");
-//       let new_json = JSON.parse(result);
-//       new_item.innerText = new_item;
-//       console.log(new_json);
-//     });
+async function get_orders(){
+  await fetch(`/orders`, {
+  			method: "GET",
+  			headers: {
+  				"Content-Type": "application/json",
+  			},
+  		})
+      .then((response) => response.text())
+  		.then((result) => {
+        let new_item = document.getElementById("new_item");
+        let new_json = JSON.parse(result);
+        new_item.innerText = new_item;
+        console.log(new_json);
+      });
+}
+get_orders();
 
 
-let plus_1 = document.getElementById("+1");
-let minus_1 = document.getElementById("-1");
-let plus_2 = document.getElementById("+2");
-let minus_2 = document.getElementById("-2");
-let count_1 = document.getElementById("count1");
-let count_2 = document.getElementById("count1");
-let amt1 = document.getElementById("amt1");
-let amt2 = document.getElementById("amt2");
-
-plus_1.addEventListener('click', () => {
-  let temp_price = parseInt(amt1.value);
-  count_1.value = parseInt(count_1.value) + 1;
-  amt1.value = temp_price*count_1.value;
-});
-
-plus_2.addEventListener('click', () => {
-  let temp_price = parseInt(amt2.value);
-  count_2.value = parseInt(count_2.value) + 1;
-  amt2.value = temp_price*count_2.value;
-});
-
-minus_1.addEventListener('click', () => {
-  let temp_price = parseInt(amt1.value);
-  count_1.value = parseInt(count_1.value) - 1;
-  amt1.value = temp_price*count_1.value;
-});
-
-minus_2.addEventListener('click', () => {
-  let temp_price = parseInt(amt2.value);
-  count_2.value = parseInt(count_2.value) + 1;
-  amt2.value = temp_price*count_2.value;
-});
+// let plus_1 = document.getElementById("+1");
+// let minus_1 = document.getElementById("-1");
+// let plus_2 = document.getElementById("+2");
+// let minus_2 = document.getElementById("-2");
+// let count_1 = document.getElementById("count1");
+// let count_2 = document.getElementById("count1");
+// let amt1 = document.getElementById("amt1");
+// let amt2 = document.getElementById("amt2");
+//
+// plus_1.addEventListener('click', () => {
+//   let temp_price = parseInt(amt1.value);
+//   count_1.value = parseInt(count_1.value) + 1;
+//   amt1.value = temp_price*count_1.value;
+// });
+//
+// plus_2.addEventListener('click', () => {
+//   let temp_price = parseInt(amt2.value);
+//   count_2.value = parseInt(count_2.value) + 1;
+//   amt2.value = temp_price*count_2.value;
+// });
+//
+// minus_1.addEventListener('click', () => {
+//   let temp_price = parseInt(amt1.value);
+//   count_1.value = parseInt(count_1.value) - 1;
+//   amt1.value = temp_price*count_1.value;
+// });
+//
+// minus_2.addEventListener('click', () => {
+//   let temp_price = parseInt(amt2.value);
+//   count_2.value = parseInt(count_2.value) + 1;
+//   amt2.value = temp_price*count_2.value;
+// });
