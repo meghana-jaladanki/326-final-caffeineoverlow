afb5a129e02e6f76f94408bfb24296b8fd3cf0d8
rinijaraja@gmail.com <73535868+rinraja@users.noreply.github.com>
2022-05-06 02:42:56 -0400

    created database.js file and changed server.js for order and customer

---
+++ database.js
@@ -0,0 +1,98 @@
+import 'dotenv/config';
+import pg from 'pg';
+
+// get pool class from the pg module
+const { Pool } = pg;
+
+export class CaffeineDatabase {
+    constructor(dburl){
+        this.dburl = dburl;
+    }
+
+    async connect(){
+        this.pool = new Pool({
+            connectionString: this.dburl,
+            ssl: {rejectUnauthorized: false},
+        });
+
+        this.client = await this.pool.connect();
+
+        // init the database
+        // await this.init_customer();
+        // await this.init_order();
+    }
+
+    // // used to initialize customer table in database
+    // async init_customer() {
+    //     const queryText = `
+    //     create table if not exists customer (
+    //         customer_id integer primary key,
+    //         username varchar(30),
+    //         password varchar(30)
+    //     );
+      
+    //     insert into 
+    //         customer(customer_id, username, password) 
+    //     values 
+    //         (2, 'Sally', 'Test1234!');
+    //     `;
+    //     const res = await this.client.query(queryText);
+    // }
+
+    // used to initialize order table in database
+    // async init_order() {
+    //     const queryText = `
+    //     create table if not exists order (
+    //         order_id integer primary key, 
+    //         drink varchar(30), 
+    //         dairy varchar(30), 
+    //         espresso varchar(30), 
+    //         flavor varchar(30), 
+    //         sweetener varchar(30), 
+    //         total integer
+    //     );
+      
+    //     insert into 
+    //         order(order_id, drink, dairy, espresso, flavor, sweetener, total) 
+    //     values 
+    //         (11, 'Latte', 'Milk', '1', 'Vanilla', 'Sugar', 5);
+    //     `;
+    //     const res = await this.client.query(queryText);
+    // }
+
+    // close the pool
+    async close(){
+        this.client.release();
+        await this.pool.end();
+    }
+
+    // CREATE/POST newCustomer
+    async createCustomer(customer_id, username, password) {
+        const queryText =
+        'INSERT INTO customer (customer_id, username, password) VALUES ($1, $2, $3) RETURNING *;';
+        const res = await this.client.query(queryText, [customer_id, username, password]);
+        return res.rows;
+    }
+
+    // CREATE/POST newOrder
+    async createOrder(order_id, drink, dairy, espresso, flavor, sweetener, total) {
+        const queryText =
+        'INSERT INTO order (order_id, drink, dairy, espresso, flavor, sweetener, total) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;';
+        const res = await this.client.query(queryText, [order_id, drink, dairy, espresso, flavor, sweetener, total]);
+        return res.rows;
+    }
+
+    // GET/READ viewCustomer
+    async readCustomer() {
+        const queryText = 'SELECT * FROM customer;';
+        const res = await this.client.query(queryText);
+        return res.rows;
+    }
+
+    // GET/READ viewOrder
+    async readOrder() {
+        const queryText = 'SELECT * FROM order;';
+        const res = await this.client.query(queryText);
+        return res.rows;
+    }
+}
--- server.js
+++ server.js
@@ -1,142 +1,110 @@
-"use strict";
-const express = require("express");
-let fs = require("fs");
-const path = require("path");
-
-const app = express();
-const port = 3000;
-
-// NEW: Add json and urlencoded middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static("client/"));
-
-let data = {};
-let filename = "data.json";
-let userID = 0;
-data = JSON.parse(fs.readFileSync(filename));
-
-// READ Endpoints
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/html/login.html"));
-});
-
-app.get("/menu", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/html/menu.html"));
-});
-
-app.get("/orders", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/html/orders.html"));
-});
-
-app.get("/options", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/html/options.html"));
-});
-
-app.get("/drink/view", (req, res) => {
-  const drinkVal = req.query["ID"];
-  for (let i = 0; i < data["drink"].length; ++i) {
-    if (drinkVal === JSON.stringify(data["drink"][i].ID)) {
-      console.log(data["drink"][i]);
-      res.send(data["drink"][i]);
-    }
+import express from "express";
+import { CaffeineDatabase } from "./database.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+class CaffeineServer {
+  constructor(dburl) {
+    this.dburl = dburl;
+    this.app = express();
+    this.app.use("/", express.static("client"));
   }
-  res.send();
-});
 
-app.get("/orders/view", (req, res) => {
-  const orderVal = req.query["ID"];
-  for (let i = 0; i < data["orders"].length; ++i) {
-    if (orderVal === JSON.stringify(data["orders"][i].ID)) {
-      console.log(data["orders"][i]);
-      res.send(data["orders"][i]);
-    }
+  async initRoutes() {
+    const self = this;
+
+    // GET/READ endpoints
+    this.app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/html/login.html"));
+    });
+    
+    this.app.get("/menu", (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/html/menu.html"));
+    });
+    
+    this.app.get("/orders", (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/html/orders.html"));
+    });
+    
+    this.app.get("/options", (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/html/options.html"));
+    });
+    
+    this.app.get("/viewCustomer", async (req, res) => {
+      try {
+        const customer = await self.db.readCustomer();
+        res.send(JSON.stringify(customer));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    this.app.get("/viewOrder", async (req, res) => {
+      try {
+        const order = await self.db.readOrder();
+        res.send(JSON.stringify(order));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    // this.app.get("/login", async (req, res) => {
+    //   try {
+    //     const words = await self.db.readWord();
+    //     res.send(JSON.stringify(words));
+    //   } catch (err) {
+    //     res.status(500).send(err);
+    //   }
+    // });
+
+    // this.app.get("/logout", async (req, res) => {
+    //   try {
+    //     const words = await self.db.readWord();
+    //     res.send(JSON.stringify(words));
+    //   } catch (err) {
+    //     res.status(500).send(err);
+    //   }
+    // });
+
+    // CREATE/POST endpoints
+    this.app.post("/newCustomer", async (req, res) => {
+      try {
+        const { customer_id, username, password } = req.query;
+        const customer = await self.db.createCustomer(customer_id, username, password);
+        res.send(JSON.stringify(customer));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    this.app.post("/newOrder", async (req, res) => {
+      try {
+        const { order_id, drink, dairy, espresso, flavor, sweetener, total } = req.query;
+        const order = await self.db.createOrder(order_id, drink, dairy, espresso, flavor, sweetener, total);
+        res.send(JSON.stringify(order));
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
   }
-  res.send();
-});
 
-app.get("/login", (req, res) => {
-  res.send(JSON.stringify({ userID: userID }));
-});
-
-app.get("/logout", (req, res) => {
-  console.log("Able to logout!");
-  res.redirect("/");
-});
-
-app.post("/signup", (req, res) => {
-  // Create a new user account
-  let user = req.body.user;
-  user["userID"] = userID;
-  userID++;
-  data["user"].push(req.body.user);
-  let userVal = JSON.stringify(data);
-  fs.writeFileSync(filename, userVal);
-  console.log("A new user account was created!");
-  res.send(JSON.stringify({ userID: userID }));
-});
-
-// CREATE Endpoints
-app.post("/drink/new", (req, res) => {
-  // Create a new drink
-  data["drink"].push(req.body.drink);
-  let drinkVal = JSON.stringify(data);
-  fs.writeFileSync(filename, drinkVal);
-  console.log("A new drink was created!");
-  res.sendStatus(200);
-});
-
-app.post("/user/new", (req, res) => {
-  // Create a new user account
-  data["user"].push(req.body.user);
-  let userVal = JSON.stringify(data);
-  fs.writeFileSync(filename, userVal);
-  console.log("A new user account was created!");
-  res.sendStatus(200);
-});
-
-app.post("/orders/new", (req, res) => {
-  // Create a new order
-  data["orders"].push(req.body.orders);
-  let ordersVal = JSON.stringify(data);
-  fs.writeFileSync(filename, ordersVal);
-  console.log("A new order was created!");
-  res.sendStatus(200);
-});
-
-// UPDATE Endpoints
-app.get("/orders/update", (req, res) => {
-  console.log("Order info has been updated!");
-  res.send();
-});
-
-// DELETE Endpoints
-app.delete("/orders/:ID", (req, res, next) => {
-  const delIndex = getIndexById(req.params.id, expressions);
-  if (delIndex !== -1) {
-    expressions.splice(delIndex, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).send();
+  async initDb() {
+    this.db = new CaffeineDatabase(this.dburl);
+    await this.db.connect();
   }
-});
 
-app.delete("/user/:ID", (req, res, next) => {
-  const delIndex = getIndexById(req.params.id, expressions);
-  if (delIndex !== -1) {
-    expressions.splice(delIndex, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).send();
+  async start() {
+    await this.initRoutes();
+    await this.initDb();
+    const port = process.env.PORT || 3000;
+    this.app.listen(port, () => {
+      console.log(`Caffeine Overflow Server listening on port ${port}!`);
+    });
   }
-});
+}
 
-app.get("*", (req, res) => {
-  console.log(req.path);
-  res.status(404).json({ message: "Unknown Request" });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+const server = new CaffeineServer(process.env.DATABASE_URL);
+server.start();
